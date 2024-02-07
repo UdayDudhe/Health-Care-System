ae0b541632c16fc7d2097e1ef246e5b8300d499a
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-02-07 21:52:00 +0530

    added bootstrap to index and added css  to login page

--- React/my-react-app/src/LoginPage/LoginPage.jsx
+++ React/my-react-app/src/LoginPage/LoginPage.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./LoginPage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function LoginPage() {
   return (
     <>
-      <div>LoginPage</div>
-    <form>
-      <div>
-        <label htmlFor="">User id:</label>
-        <input type="text" name="" id="" />
-        <br />
-        <label htmlFor="">Password</label>
-        <input type="password" name="" id="" />
-      </div>
+      <div className="form-Container">
+        <legend>Login</legend>
 
-    </form>
+        <form>
+          <div className="form-group">
+            <label htmlFor="">User id</label>
+            <input type="text" name="" id="" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="">Password</label>
+            <input type="password" name="" id="" className="form-control" />
+          </div>
+
+          <button type="" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
     </>
-  
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
