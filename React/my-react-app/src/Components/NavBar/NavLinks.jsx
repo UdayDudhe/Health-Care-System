d937dd89d49390c83ef3fc33a3af217e22589544
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-02-08 18:35:51 +0530

    added navigation bar with links

---
+++ React/my-react-app/src/Components/NavBar/NavLinks.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./NavLinks.css";
+
+function NavLinks() {
+  const location = useLocation();
+
+  return (
+    <>
+      <nav class="navbar navbar-expand-lg sticky-top  navbar-dark bg-dark">
+        <div>
+          <Link className="navbar-brand" to="/">
+            <i className="bi bi-heart-pulse-fill"></i> HealthCare System
+          </Link>
+        </div>
+
+        <div>
+          {location.pathname === "/" && (
+            <>
+              <Link className="navbar-text" to="/doc-reg">
+                Register Doctor
+              </Link>
+              <Link className="navbar-text" to="/doc-reg">
+                Register Patient
+              </Link>
+          
+            </>
+          )}
+          {location.pathname === "/doc-reg" && (
+            <>
+               <Link className="navbar-text" to="/">
+                Login
+              </Link>
+            </>
+          )}
+          {location.pathname === "/pat-reg" && (
+            <>
+              <Link className="navbar-text" to="/">
+                Login
+              </Link>
+            </>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavLinks;
