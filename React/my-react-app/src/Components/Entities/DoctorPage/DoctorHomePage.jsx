c7027fe1dd86e372e89363a5ed78381ea17ccd78
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-02-12 01:41:33 +0530

    Added navigation based on login data

---
+++ React/my-react-app/src/Components/Entities/DoctorPage/DoctorHomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DoctorHomePage() {
+  return (
+    <div>DoctorPage</div>
+  )
+}
+
+export default DoctorHomePage
--- React/my-react-app/src/Components/LoginPage/LoginPage.jsx
+++ React/my-react-app/src/Components/LoginPage/LoginPage.jsx
@@ -1,25 +1,112 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "./LoginPage.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  //inital object
+  const init = {
+    username: "",
+    password: "",
+  };
+
+  //function for dispatcher
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "update":
+        return { ...state, [action.fld]: action.val };
+      case "reset":
+        return init;
+      default:
+    }
+  };
+
+  const navigate = useNavigate();
+
+  //dispatch to modify info object
+  const [info, dispatch] = useReducer(reducer, init);
+  const [msg, setMsg] = useState("");
+  const sendData = (e) => {
+    e.preventDefault();
+    const reqOptions = {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(info),
+    };
+    fetch("http://localhost:8080/verifyLogin", reqOptions)
+      // .then((resp) => console.log(resp))
+      .then((resp) => resp.text())
+      .then((text) => (text.length ? JSON.parse(text) : {}))
+      .then((obj) => {
+        if (Object.keys(obj).length === 0) {
+          setMsg("Account not found");
+        } else {
+          if (obj.id_approved === false) {
+            alert("req not approved");
+          } else {
+            if (obj.role_id.roleid === 1) {
+              navigate('/adminHome')
+            } else if (obj.role_id.roleid === 2) {
+              navigate('/docHome')
+            } else if (obj.role_id.roleid === 3) {
+              navigate('/patientHome')
+
+            }
+          }
+        }
+      });
+      //.then((obj)=>{console.log(obj)})
+  };
+
   return (
     <>
       <div className="form-Container">
         <span id="login-label">Login</span>
 
-        <form>
+        <form method="POST">
           <div className="form-group">
             <label htmlFor="">User id</label>
-            <input type="text" name="" id="" className="form-control" />
+            <input
+              type="text"
+              name="username"
+              id=""
+              className="form-control"
+              value={info.username}
+              onChange={(e) => {
+                dispatch({ type: "update", fld: "username", val: e.target.value });
+              }}
+            />
           </div>
 
           <div className="form-group">
             <label htmlFor="">Password</label>
-            <input type="password" name="" id="" className="form-control" />
+            <input
+              type="password"
+              name="password"
+              id=""
+              className="form-control"
+              value={info.password}
+              onChange={(e) => {
+                dispatch({ type: "update", fld: "password", val: e.target.value });
+              }}
+            />
           </div>
+          {msg}
 
-          <button type="" className="btn btn-primary">Submit</button>
+          <button
+            type=""
+            className="btn btn-primary"
+            onClick={(e) => sendData(e)}
+          >
+            Submit
+          </button>
+          <button
+            type=""
+            className="btn btn-primary"
+            onClick={() => dispatch({ type: "reset" })}
+          >
+            Reset
+          </button>
         </form>
       </div>
     </>
