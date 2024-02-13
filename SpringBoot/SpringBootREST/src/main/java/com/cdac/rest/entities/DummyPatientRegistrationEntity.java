2fd5c78bf71e907b3251ed4692b40dad422b4f47
rushi6298 <rwaikar4@gmail.com>
2024-02-13 15:31:15 +0530

    added patient registration - springboot JPA

---
+++ SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/entities/DummyPatientRegistrationEntity.java
@@ -0,0 +1,123 @@
+package com.cdac.rest.entities;
+
+
+public class DummyPatientRegistrationEntity 
+{
+	int login_id;
+	String username;
+	String password;
+	int role_id;
+	boolean is_approved;
+	int patient_id;
+	String first_name;
+	String last_name;
+	String adress;
+	long phone_number;
+	String gender;
+	String email_id;
+	String city;
+	String state;
+	long pincode; 
+	int patient_login_id;
+	public int getLogin_id() {
+		return login_id;
+	}
+	public void setLogin_id(int login_id) {
+		this.login_id = login_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getRole_id() {
+		return role_id;
+	}
+	public void setRole_id(int role_id) {
+		this.role_id = role_id;
+	}
+	public boolean isIs_approved() {
+		return is_approved;
+	}
+	public void setIs_approved(boolean is_approved) {
+		this.is_approved = is_approved;
+	}
+	public int getPatient_id() {
+		return patient_id;
+	}
+	public void setPatient_id(int patient_id) {
+		this.patient_id = patient_id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public long getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(long phone_number) {
+		this.phone_number = phone_number;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail_id() {
+		return email_id;
+	}
+	public void setEmail_id(String email_id) {
+		this.email_id = email_id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public long getPincode() {
+		return pincode;
+	}
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
+	public int getPatient_login_id() {
+		return patient_login_id;
+	}
+	public void setPatient_login_id(int patient_login_id) {
+		this.patient_login_id = patient_login_id;
+	}
+	
+	
+	
+	
+
+}
