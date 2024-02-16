bb7550bade9d847723600920a0cebb5f5e3aac68
75007-Uday <udaydudhe312@gmail.com>
2024-02-16 19:30:06 +0530

    updated patient registration form - successful registration

--- SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/controllers/PatientRegistrationController.java
+++ SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/controllers/PatientRegistrationController.java
@@ -3,6 +3,7 @@ package com.cdac.rest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,7 +18,7 @@ import com.cdac.rest.services.PatientRegistrationService;
 import com.cdac.rest.services.RoleService;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class PatientRegistrationController 
 {
 	@Autowired
