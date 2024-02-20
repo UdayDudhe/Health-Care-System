68e158c7b8b6e094eb212a06526cc72c2f4d3a76
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-02-21 04:43:53 +0530

    added doctors in patient home page

--- React/my-react-app/src/Components/Entities/PatientPage/PatientHomePage.jsx
+++ React/my-react-app/src/Components/Entities/PatientPage/PatientHomePage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { React, useEffect, useState } from "react";
+import AdminDoctorEntity from "./DoctorEntity";
+
 
 function PatientHomePage() {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/getAllDoctor")
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        } else {
+          throw new Error("Failed to fetch doctors");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setDoctors(data);
+      })
+      .catch((error) => {
+        alert("Error fetching doctors:", error);
+      });
+  }, []);
+
+  const card = doctors.map((item) => <AdminDoctorEntity {...item} />);
+
   return (
-    <div>PatientPage</div>
-  )
+    <>
+      <div className="PatientHomePage">
+        <legend></legend>
+        <div className="container w-100">
+          <div className="row">
+            {card.map((value) => (
+              <div className="col-lg-6"> {value} </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default PatientHomePage
+export default PatientHomePage;
