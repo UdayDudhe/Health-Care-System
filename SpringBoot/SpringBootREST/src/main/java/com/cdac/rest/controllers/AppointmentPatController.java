609cf22d4808033a8f48e252bec3430fa1e38022
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-02-22 12:00:59 +0530

    rest api for prescription

--- SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/controllers/AppointmentPatController.java
+++ SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/controllers/AppointmentPatController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,7 @@ import com.cdac.rest.entities.Appointment;
 import com.cdac.rest.entities.AppointmentPatientAssociationEntity;
 import com.cdac.rest.entities.DoctorRegistrationEntity;
 import com.cdac.rest.repositories.AppointmentPatRepository;
+import com.cdac.rest.services.AppointmentPatService;
 import com.cdac.rest.services.DoctorRegistrationService;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -28,6 +31,9 @@ public class AppointmentPatController {
 	@Autowired
 	AppointmentPatRepository appointmentPatService;
 	
+	@Autowired
+	private AppointmentPatService appointmentService;
+	
 	   @GetMapping("/getAscAppointments/{doctorId}/{appointmentDate}")
 	    public ResponseEntity<List<AppointmentPatientAssociationEntity>> getAppointmentsByDateAndDoctorId(
 	    		@PathVariable("doctorId") Integer doctorId,
@@ -37,6 +43,19 @@ public class AppointmentPatController {
 	         doctorId= doctor.getDoctorid();
 	        List<AppointmentPatientAssociationEntity> appointments = appointmentPatService.findByDoctorIdAndAppointmentDate(doctorId, appointmentDate);
 	        return new ResponseEntity<>(appointments, HttpStatus.OK);
+	    }
+	   
+	   @PutMapping("/{id}/prescription")
+	    public ResponseEntity<AppointmentPatientAssociationEntity> updatePrescription(
+	            @PathVariable("id") Integer id,
+	            @RequestBody String prescription) {
+
+	        AppointmentPatientAssociationEntity updatedAppointment = appointmentService.updatePrescription(id, prescription);
+	        if (updatedAppointment != null) {
+	            return new ResponseEntity<>(updatedAppointment, HttpStatus.OK);
+	        } else {
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
 	    }
 
 }
--- SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/services/AppointmentPatService.java
+++ SpringBoot/SpringBootREST/src/main/java/com/cdac/rest/services/AppointmentPatService.java
@@ -11,14 +11,24 @@ import com.cdac.rest.repositories.AppointmentPatRepository;
 @Service
 public class AppointmentPatService {
 	
+	@Autowired
 	private final AppointmentPatRepository appointmentPatRepository;
 	
-	@Autowired
 	  public AppointmentPatService(AppointmentPatRepository appointmentRepository) {
         this.appointmentPatRepository = appointmentRepository;
     }
 
 	 public List<AppointmentPatientAssociationEntity> getAppointmentsByDoctorIdAndDate(Integer doctorId, String appointmentDate) {
 	        return appointmentPatRepository.findByDoctorIdAndAppointmentDate(doctorId, appointmentDate);
+	    }
+	 
+
+	    public AppointmentPatientAssociationEntity updatePrescription(Integer id, String prescription) {
+	        AppointmentPatientAssociationEntity appointment = appointmentPatRepository.findById(id).orElse(null);
+	        if (appointment != null) {
+	            appointment.setPrescription(prescription);
+	            return appointmentPatRepository.save(appointment);
+	        }
+	        return null; 
 	    }
 }
